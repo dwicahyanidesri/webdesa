ad726829c822139456fe9c1dfe5f1c44e3d2f593
dwicahyanidesri-blip <dwicahyanidesri@gmail.com>
2026-07-22 12:12:57 +0700

    revoke

--- database/seeders/ProfilDesaSeeder.php
+++ database/seeders/ProfilDesaSeeder.php
@@ -13,9 +13,9 @@ class ProfilDesaSeeder extends Seeder
             ['id' => 1],
             [
                 'nama_desa' => 'Tanjung Agung',
-                'kecamatan' => 'Kecamatan Tanjung Agung',
-                'kabupaten' => 'Kabupaten',
-                'provinsi' => 'Provinsi',
+                'kecamatan' => 'Teluk Pandan',
+                'kabupaten' => 'Pesawaran',
+                'provinsi' => 'Lampung',
                 'sejarah' => 'Desa Tanjung Agung merupakan salah satu desa yang memiliki sejarah panjang dalam perkembangan wilayahnya. Silakan lengkapi narasi sejarah desa melalui menu admin.',
                 'visi' => 'Mewujudkan Desa Tanjung Agung yang mandiri, sejahtera, dan berbudaya.',
                 'misi' => "1. Meningkatkan kualitas sumber daya manusia.\n2. Mengembangkan potensi ekonomi desa secara berkelanjutan.\n3. Meningkatkan pelayanan publik yang transparan dan akuntabel.\n4. Menjaga kelestarian lingkungan dan budaya lokal.",
@@ -23,10 +23,10 @@ class ProfilDesaSeeder extends Seeder
                 'jumlah_penduduk' => '0 Jiwa',
                 'nama_kontak' => 'Kepala Desa Tanjung Agung',
                 'jabatan_kontak' => 'Kepala Desa',
-                'no_telepon' => '0800-0000-0000',
-                'email' => 'desa.tanjungagung@example.id',
+                'no_telepon' => '0878-8289-3844',
+                'email' => 'pemerintahdesatanjungagung@gmail.com',
                 'alamat_kantor' => 'Kantor Desa Tanjung Agung',
-                'jam_pelayanan' => 'Senin - Jumat, 08.00 - 15.00 WIB',
+                'jam_pelayanan' => 'Senin - Jumat, 08.00 - 14.00 WIB',
                 'link_maps' => null,
             ]
         );
